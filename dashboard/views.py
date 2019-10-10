e88ce813311d49c73e3c45b33c4cd85f9a75835a
Dominik Resl <nikicresl@gmail.com>
2019-10-10 02:47:16 +0200

    Fix view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponseForbidden, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .models import Service
 
