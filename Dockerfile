e8dc5778517805d370caf93db797ca1846bb797a
Dominik Resl <nikicresl@gmail.com>
2020-02-02 11:35:23 +0100

    Update docker

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ COPY requirements.txt /code/
 COPY manage.py /code/
 RUN pip install -r requirements.txt
 COPY . /code/
+
+CMD ['gunicorn --bind 0.0.0.0:37803 rewiza_services.wsgi']
--- rewiza_services/urls.py
+++ rewiza_services/urls.py
@@ -7,6 +7,8 @@ from django.urls import include, path, re_path
 from dashboard import views as dashboard_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('', include('dashboard.urls', namespace="dashboard")),
@@ -16,3 +18,5 @@ urlpatterns = [
     path('v2/service/auth/', dashboard_views.service_auth_check),
     re_path(r'^idp/redirect/(?P<value>\w+)', dashboard_views.idp_redirect, name='idp_redirect'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
